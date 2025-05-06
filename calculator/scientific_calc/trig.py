3a5c8aaeb205f9dc325446547dab48a4a06e1386
darshan-1956 <darshan94432@gmail.com>
2025-05-06 20:29:31 +0800

    Create trig.py

---
+++ calculator/scientific_calc/trig.py
@@ -0,0 +1,13 @@
+import math as m
+def sin(x):
+ return m.sin(x)
+def cos(x):
+ return m.cos(x)
+def tan(x):
+ return m.tan(x)
+def root(x):
+ return m.sqrt(x)
+def degrees(x):
+ return m.degrees(x)
+def radians(x):
+ return m.radians(x)
