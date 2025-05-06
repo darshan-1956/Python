fac4e01acc277170ee5a015c35b48cb6714c1ad6
darshan-1956 <darshan94432@gmail.com>
2025-05-06 20:14:12 +0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import product
-import sales
-product.add_product(101, "Notebook", 50.0, 100)
-product.add_product(102, "Pen", 10.0, 200)
-print(product.get_product_info(101))
-print(sales.sell_product(101, 2))
-print(sales.sell_product(102, 5))
-print(product.get_product_info(101))
-print(sales.show_sales())
